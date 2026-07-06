e0563587bedd6ecd0266298cd5f49eacedcbc282
Sauronin <serhiisaunin@gmail.com>
2026-07-06 22:04:38 +0300

    Add main menu screen with Play button

---
+++ scripts/MainMenu.gd
@@ -0,0 +1,8 @@
+﻿extends Control
+
+func _ready():
+	$CenterContainer/VBoxContainer/PlayButton.pressed.connect(_on_play_pressed)
+
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://scenes/Main.tscn")
